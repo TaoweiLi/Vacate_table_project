6f7942d2241e576de693dbc3fd8e9a28c5a907eb
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-09-05 00:28:36 -0700

    update frontend auth code

--- vtable_frontend/src/index.js
+++ vtable_frontend/src/index.js
@@ -2,30 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider } from './context/Modal';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
+
+// const Carrot = () => (
+//   <div style={{ color: "orange", fontSize: "100px" }}>
+//     <i className="fa-solid fa-carrot"></i>
+//   </div>
+// );
 
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          {/* <Carrot /> */}
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
