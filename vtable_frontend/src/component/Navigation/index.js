d9717a43ac849e9412eb2f6f0c7affeace9defbd
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-09-06 00:16:52 -0700

    updated nav bar

--- vtable_frontend/src/component/Navigation/index.js
+++ vtable_frontend/src/component/Navigation/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import './Navigation.scss';
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
@@ -10,24 +10,41 @@ function Navigation() {
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
+      <div className='nav-signout-state'>
+        <ProfileButton user={sessionUser} />
+      </div>
+      
+    )
   } else {
     sessionLinks = (
-      <>
+      <div className='nav-signout-state'>
         <NavLink to="/signin">Sign In</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
-      </>
-    );
+      </div>
+    )
   }
 
+
+
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Vacate Table</NavLink>
-        {sessionLinks}
-      </li>
-    </ul>
+    <>
+      <div className="nav-bar">
+        <div className="nav-left">
+          <a className="nav-logo" href="/">
+            <img className="nav-logo-img" src="" alt="Vacate Table Logo"></img>
+          </a>
+          <div className='nav-location-menu'>
+            <i className="fa-solid fa-location-dot"></i>
+          </div>
+        </div>
+        <div className="nav-right">
+          <div className='nav-user-state'>{sessionLinks}</div>
+          <div className='nav-seach'>
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
