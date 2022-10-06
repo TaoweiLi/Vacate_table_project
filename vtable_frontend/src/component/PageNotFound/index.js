3f942459b2e09af551fc417db1043e031d0cdd5b
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-10-06 00:24:00 -0700

    update page not found css

--- vtable_frontend/src/component/PageNotFound/index.js
+++ vtable_frontend/src/component/PageNotFound/index.js
@@ -1,12 +1,12 @@
-
+import "./PageNotFound.scss";
 
 function PageNotFound() {
 
   return(
-    <>
+    <div className="page-wrapper">
     <h1>404</h1>
     <h2>PageNotFound</h2>
-    </>
+    </div>
   )
 }
 
